45d38ef349b2506689eef040cb793dd20ef1cea0
“63walaxe” <63walaxe@stud.akademiet.no>
2026-09-04 15:04:22 +0200

    opprettet en funksjon "vis_alle()" som skriver ut navn og nummer i begge ordbøker i en for løkke. Og utførte funksjonen.

--- python.py
+++ python.py
@@ -1,11 +1,16 @@
 telefonbok = []
 person1 = {
-    "navn":"sverrre",
-    "nummer":"+47 46576760"
+    "navn": "sverrre",
+    "nummer": "46576760"
 }
 person2 = {
-    "navn":"andreas",
-    "nummer":"+47 "
+    "navn": "andreas",
+    "nummer": "93692597"
 }
-telefonbok.append(person1["navn"])
-telefonbok.append(person2["navn"])
+telefonbok.append(person1)
+telefonbok.append(person2)
+
+def vis_alle():
+    for person in telefonbok:
+        print (f"Navn: {person["navn"]}, Nummer: {person["nummer"]}")
+vis_alle()
